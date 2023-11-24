5ee726506aa7b93b88d0ee04498c7783d5fcfaed
Devin Zhang <Devin@HandeMacBook-Pro.local>
2023-11-24 00:29:59 -0600

    Set up app with Firebase, and implemented front and backend of signup and login using Firebase authentication and database.

---
+++ Fix/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,121 @@
+//
+//  SignUpViewController.swift
+//  Fix
+//
+//  Created by Devin on 2023/11/23.
+//
+
+import UIKit
+import FirebaseAuth
+import Firebase
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passWordTextField: UITextField!
+    
+    @IBOutlet weak var comfirmPasswordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        errorLabel.alpha = 0
+    }
+    
+    // Check fields before navigating to more info screen. Show error message if needed.
+    @IBAction func signUpTapped(_ sender: Any) {
+        
+        // validate SignUp screen's information fields, print error if needed
+        if isTextFieldEmpty(firstNameTextField) || isTextFieldEmpty(lastNameTextField){
+            showError(errorLabel, "Enter your first and last name")
+            return
+        }
+        if isTextFieldEmpty(emailTextField) {
+            showError(errorLabel, "Enter your email address")
+            return
+        }
+        if let email = emailTextField.text, !isValidEmailAddr(strToValidate: email) {
+            showError(errorLabel, "Enter a correct email address")
+            return
+        }
+        if isTextFieldEmpty(passWordTextField) {
+            showError(errorLabel, "Enter a passord to use")
+            return
+        }
+        if let password = passWordTextField.text, !isPasswordValid(password) {
+            showError(errorLabel, "Password need to contain at least one digit, one upper case letter, one lower case letter, and be 8 characters long")
+            return
+        }
+        if isTextFieldEmpty(comfirmPasswordTextField) {
+            showError(errorLabel, "Comfirm your password")
+            return
+        }
+        if let pw = passWordTextField.text, let cpw = comfirmPasswordTextField.text, pw != cpw {
+            showError(errorLabel, "Your password comfirmation does not match your password")
+            return
+        }
+        
+        errorLabel.alpha = 0
+        
+        let firstName = firstNameTextField.text!
+        let lastName = lastNameTextField.text!
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passWordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // create user
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                self.showError(self.errorLabel, "Error creating user: \(error.localizedDescription)")
+            } else {
+                // success creating user, store user info to database
+                let db = Firestore.firestore()
+                db.collection("users").addDocument(data: ["first_name": firstName, "last_name": lastName, "uid": authResult!.user.uid]) { error in
+                    if let error = error {
+                        self.showError(self.errorLabel, "User created but failed to save data to db: \(error.localizedDescription)")
+                    }
+                }
+                self.transitonToUserEnvironment()
+            }
+        }
+    }
+    
+    // Go to userEnvironment
+    func transitonToUserEnvironment() {
+        
+    }
+    
+    
+    // Helper functions
+    func showError(_ label: UILabel, _ errorMessage: String) {
+        label.text = errorMessage
+        label.alpha = 1
+    }
+    
+    func isTextFieldEmpty(_ textField: UITextField) -> Bool {
+        return textField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""
+    }
+    
+    func isValidEmailAddr(strToValidate: String) -> Bool {
+      let emailValidationRegex = "^[\\p{L}0-9!#$%&'*+\\/=?^_`{|}~-][\\p{L}0-9.!#$%&'*+\\/=?^_`{|}~-]{0,63}@[\\p{L}0-9-]+(?:\\.[\\p{L}0-9-]{2,7})*$"
+
+      let emailValidationPredicate = NSPredicate(format: "SELF MATCHES %@", emailValidationRegex)
+
+      return emailValidationPredicate.evaluate(with: strToValidate)
+    }
+    
+    // ensure password Is at least 8 characters long. Contains at least one lowercase letter, one uppercase letter, and one digit.
+    func isPasswordValid(_ password : String) -> Bool{
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z\\d]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+}
