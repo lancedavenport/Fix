a442b1e875b0cffe4eb5a6a9b6de266412654f0e
Brian Wu <bwu225@wisc.edu>
2023-12-12 01:10:34 -0600

    [Provide explanation for previous Push] Optimize expression, enhance user prompts, provide notes

--- Fix/Main ViewControllers/SignUpViewController.swift
+++ Fix/Main ViewControllers/SignUpViewController.swift
@@ -184,9 +184,7 @@ class SignUpViewController: UIViewController {
     // Validates an email address.
     func isValidEmailAddr(strToValidate: String) -> Bool {
       let emailValidationRegex = "^[\\p{L}0-9!#$%&'*+\\/=?^_`{|}~-][\\p{L}0-9.!#$%&'*+\\/=?^_`{|}~-]{0,63}@[\\p{L}0-9-]+(?:\\.[\\p{L}0-9-]{2,7})*$"
-
       let emailValidationPredicate = NSPredicate(format: "SELF MATCHES %@", emailValidationRegex)
-
       return emailValidationPredicate.evaluate(with: strToValidate)
     }
     
