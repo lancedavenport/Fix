0100d3bac33eb983062ab2219e37690cf0f5dc13
bwu225 <bwu225@wisc.edu>
2023-12-15 09:36:18 +0000

    Waiting for Completion

--- FixTests/AsyncTests.swift
+++ FixTests/AsyncTests.swift
@@ -3,10 +3,15 @@ import XCTest
 
 class AsyncOperationTests: XCTestCase {
     func testAsyncCallWithUnnecessarilyLongTimeout() {
+
         let expectation = XCTestExpectation(description: "Async test")
+        
         DispatchQueue.global().async {
             sleep(2)
             expectation.fulfill()
         }
+
+        wait(for: [expectation], timeout: 30)
+
     }
 }
