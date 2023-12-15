89d916f08e9aa5a307590c8fd5cd6d7714580ab5
bwu225 <bwu225@wisc.edu>
2023-12-15 09:48:40 +0000

    Add func totalSizeOfFilesInDirectory

--- FixTests/FileManagementTests.swift
+++ FixTests/FileManagementTests.swift
@@ -24,4 +24,24 @@ class FileManagementTests: XCTestCase {
     }
     }
 
+    func totalSizeOfFilesInDirectory(atPath path: String) -> UInt64 {
+    let fileManager = FileManager.default
+    var totalSize: UInt64 = 0
+
+    do {
+        let files = try fileManager.contentsOfDirectory(atPath: path)
+        for file in files {
+            let filePath = "\(path)/\(file)"
+            let attributes = try fileManager.attributesOfItem(atPath: filePath)
+            if let fileSize = attributes[.size] as? UInt64 {
+                totalSize += fileSize
+            }
+        }
+    } catch {
+        return 0
+    }
+
+    return totalSize
+    }
+
 }
