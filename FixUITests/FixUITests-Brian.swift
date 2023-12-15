8671a78ff64180327ba8999ed76cce5b8b699bd8
bwu225 <bwu225@wisc.edu>
2023-12-15 06:23:50 +0000

    Adding Logic

--- FixUITests/FixUITests-Brian.swift
+++ FixUITests/FixUITests-Brian.swift
@@ -2,4 +2,27 @@ import XCTest
 
 class MyAppUITests: XCTestCase {
 
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+        let app = XCUIApplication()
+    }
+
+    func testAnotherExample() throws {
+        let app = XCUIApplication()
+    }
+
+    func testPerformanceExample() throws {
+        measure {
+        }
+    }
+
 }
