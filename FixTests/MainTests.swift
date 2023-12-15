9212d77896db6c030d0b9cea55442f03b40110c5
bwu225 <bwu225@wisc.edu>
2023-12-15 01:54:37 +0000

    Create MainTests.swift

---
+++ FixTests/MainTests.swift
@@ -0,0 +1,21 @@
+import XCTest
+@testable import Fix
+
+class FixTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+        }
+    }
+
+}
