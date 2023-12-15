a740dbd6301bace3f979b067a3808c8b34a4e292
bwu225 <bwu225@wisc.edu>
2023-12-15 09:47:50 +0000

    Add func countFilesInDirectory

--- FixTests/FileManagementTests.swift
+++ FixTests/FileManagementTests.swift
@@ -3,5 +3,15 @@ import Foundation
 @testable import Fix
 
 class FileManagementTests: XCTestCase {
+
+    func countFilesInDirectory(atPath path: String) -> Int {
+    let fileManager = FileManager.default
+    do {
+        let files = try fileManager.contentsOfDirectory(atPath: path)
+        return files.count
+    } catch {
+        return 0
+    }
+    }
     
 }
